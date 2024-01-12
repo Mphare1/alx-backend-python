eeef01b2496aa6a193422ec25320dc8eebeaad9d
Sakkie Thipane <123340673+Mphare1@users.noreply.github.com>
2024-01-12 21:17:05 +0200

    Create 3-to_str.py

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""function that returns a string representation of a float"""
+
+
+def to_str(n: float) -> str:
+    """Returns a string representation of a float
+    Args:
+        n: float to convert to a string
+    Returns:
+        string representation of the float
+    """
+    return str(n)
